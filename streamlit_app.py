b76f4e5b23446cb5f4f9d831f61734460ad480dc
Qsten1 <145329851+Qsten1@users.noreply.github.com>
2023-09-18 10:48:54 +0200

    Update streamlit_app.py
    adding menu posts

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My parents New healthy diner')
 
 streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
+
